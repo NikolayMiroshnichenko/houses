c739e968513be4ea9f58f9c09848e88874183af5
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2020-07-16 19:12:08 +0300

    fix-achors

--- js/anchors.js
+++ js/anchors.js
@@ -1,14 +1,22 @@
-const anchors = document.querySelectorAll('.anchors')
+// const anchors = document.querySelectorAll('.anchors')
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
     
-    const blockID = anchor.getAttribute('href').substr(1);
+//     const blockID = anchor.getAttribute('href').substr(1);
     
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
+//     document.getElementById(blockID).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     })
+//   })
+// }
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+     var target = $(this).attr('href');
+     $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+     return false;
+  });
+});
