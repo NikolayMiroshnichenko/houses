c4d955e51cfd851e9dafd01125637abc1e2fbf54
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2020-07-16 19:21:38 +0300

    last

--- js/anchors.js
+++ js/anchors.js
@@ -1,18 +1,3 @@
-// const anchors = document.querySelectorAll('.anchors')
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault();
-    
-//     const blockID = anchor.getAttribute('href').substr(1);
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
-
 $(function(){
   $('a[href^="#"]').click(function(){
      var target = $(this).attr('href');
